1138d323808c06adfcc461f81765f1ce151dd52d
user <user@daou-tcwcbc>
2016-12-08 19:33:18 +0900

    161208 최병철
    분할 후 오류수정 2

--- PushO/src/server/service/OIOServer.java
+++ PushO/src/server/service/OIOServer.java
@@ -15,10 +15,10 @@ import server.res.ServerConst;
  * @author 최병철
  * @Description 서버 프로그램, OIO방식의 Socket통신, 일종의 Controller 클래스 다수의 클라이언트와의 연결을
  *              관리하는 방법으로 ArrayList<Socket>을 사용중 Socket연결 시 최초 사용자 인증은 싱글톤
- *              패턴으로 구현된 {@link AuthClientProxy}를 사용 인증이 실패했을 경우
+ *              패턴으로 구현된 {@link AuthClientHandler}를 사용 인증이 실패했을 경우
  *              {@link EmptyResultDataException}를 통해 연결 해제 인증이 끝난 후에는 실제로 각
  *              클라이언트와 통신하는 {@link ProcessCilentRequest}를 실행 TODO 멀티쓰레드를 통한
- *              다수의 클라이언트 소켓 관리(Thread pooling) {@link AuthClientProxy}가
+ *              다수의 클라이언트 소켓 관리(Thread pooling) {@link AuthClientHandler}가
  *              싱글톤이었을 경우 문제발생 여부 고려 클라이언트와 연결이 되었을 때의 초기화 작업(인증, 암호화 등)
  *              타임아웃이 발생하였을 경우 자원관리 매커니즘
  */
